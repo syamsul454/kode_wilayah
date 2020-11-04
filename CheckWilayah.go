073776018ecca66c2dab3d97fb6ba0d8df9221ba
@syamsul <syamsuldevloper@gmail.com>
2020-11-04 15:55:59 +0700

    data wilayah

--- CheckWilayah.go
+++ CheckWilayah.go
@@ -1,8 +1,30 @@
-package CheckWilayah
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
 
-func Check() {
-	fmt.Println("hello")
+	"github.com/syamsul454/kode_wilayah/dataWilayah"
+)
+
+func Check(nik int64) {
+	t := strconv.FormatInt(int64(nik), 10)
+	a := []rune(t)
+	provinsi := string(a[0:2])
+	kabupaten := string(a[2:4])
+	kecamatan := string(a[4:6])
+	ConvertProvinsi, _ := strconv.ParseInt(provinsi, 10, 64)
+	ConvertKecamatan, _ := strconv.ParseInt(provinsi+kabupaten, 10, 64)
+	fmt.Println("provinsi", dataWilayah.Propinsi[provinsi])
+	fmt.Println("kabupaten", dataWilayah.Kbtn[ConvertProvinsi][kabupaten])
+	fmt.Println("kecamatan", dataWilayah.Kecamatan[ConvertKecamatan][kecamatan])
+	fmt.Println("nomor nik", t)
 	return
+}
+
+func main() {
+	Check(520320)
+	for _, propinsi := range dataWilayah.Propinsi {
+		fmt.Println(propinsi)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/syamsul454/kode_wilayah
 
 go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.3

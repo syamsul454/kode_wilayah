415c4b2c19e429c1a53e4477c6d74cbcaa30db28
@syamsul <syamsuldevloper@gmail.com>
2020-11-05 09:44:57 +0700

    update data wilayah

--- CheckWilayah.go
+++ CheckWilayah.go
@@ -1,23 +1,22 @@
 package CheckWilayah
 
 import (
-	"fmt"
 	"strconv"
 
 	"github.com/syamsul454/kode_wilayah/dataWilayah"
 )
 
-func Check(nik int64) {
+func Check(nik int64) (string, string, string) {
 	t := strconv.FormatInt(int64(nik), 10)
 	a := []rune(t)
 	provinsi := string(a[0:2])
-	kabupaten := string(a[2:4])
-	kecamatan := string(a[4:6])
+	kbtn := string(a[2:4])
+	kcmtan := string(a[4:6])
 	ConvertProvinsi, _ := strconv.ParseInt(provinsi, 10, 64)
-	ConvertKecamatan, _ := strconv.ParseInt(provinsi+kabupaten, 10, 64)
-	fmt.Println("provinsi", dataWilayah.Propinsi[provinsi])
-	fmt.Println("kabupaten", dataWilayah.Kbtn[ConvertProvinsi][kabupaten])
-	fmt.Println("kecamatan", dataWilayah.Kecamatan[ConvertKecamatan][kecamatan])
-	fmt.Println("nomor nik", t)
-	return
+	ConvertKecamatan, _ := strconv.ParseInt(provinsi+kbtn, 10, 64)
+	propinsi := dataWilayah.Propinsi[provinsi]
+	kabupaten := dataWilayah.Kbtn[ConvertProvinsi][kbtn]
+	kecamatan := dataWilayah.Kecamatan[ConvertKecamatan][kcmtan]
+
+	return propinsi, kabupaten, kecamatan
 }
